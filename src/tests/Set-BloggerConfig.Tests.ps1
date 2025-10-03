9798286c068a7cf1228805f23e17f75e1578cedd
Bryan Cook <3217452+bryanbcook@users.noreply.github.com>
2025-10-03 22:38:57 +0000

    additional test fixes

--- src/tests/Set-BloggerConfig.Tests.ps1
+++ src/tests/Set-BloggerConfig.Tests.ps1
@@ -2,12 +2,12 @@
 $script:UserPreferences = @(
   @{ UserPreference="BlogId"; UserPreferenceValue="12345" }
   @{ UserPreference="ExcludeLabels"; UserPreferenceValue=@("personal/blog-post") }
-  @{ UserPreference="AttachmentsDirectory"; UserPreferenceValue="TestDrive:\Attachments"}
+  @{ UserPreference="AttachmentsDirectory"; UserPreferenceValue="TestDrive:/Attachments"}
 )
 
 Describe "Set-BloggerConfig" {
   BeforeEach {
-    Import-Module $PSScriptRoot\..\PSBlogger.psm1 -Force
+    Import-Module $PSScriptRoot/../PSBlogger.psm1 -Force
     
     # Ensure the test has a clean BloggerSession variable
     InModuleScope "PSBlogger" {
@@ -28,7 +28,7 @@ Describe "Set-BloggerConfig" {
       New-Variable -Name BloggerSession -Value $BloggerSession -Scope Script -Force
     }
 
-    New-Item -Path TestDrive:\Attachments -ItemType Directory -Force | Out-Null
+    New-Item -Path TestDrive:/Attachments -ItemType Directory -Force | Out-Null
   }
 
   It "Should persist new value to <UserPreference> to BloggerSession.UserPreferences" -TestCases $script:UserPreferences {
@@ -71,7 +71,7 @@ Describe "Set-BloggerConfig" {
   It "Should enforce the AttachmentsDirectory is a valid path" {
     # act
     {
-      Set-BloggerConfig -Name AttachmentsDirectory -Value ".\InvalidPath"
+      Set-BloggerConfig -Name AttachmentsDirectory -Value "./InvalidPath"
     } | Should -Throw
   }
 
@@ -79,15 +79,15 @@ Describe "Set-BloggerConfig" {
     
     try {
       # arrange
-      $relativePath = ".\Attachments"
-      Push-Location TestDrive:\      
+      $relativePath = "./Attachments"
+      Push-Location TestDrive:/      
 
       # act
       Set-BloggerConfig -Name AttachmentsDirectory -Value $relativePath
 
       # assert
       InModuleScope "PSBlogger" {
-        $expectedPath = (Resolve-Path -Path "TestDrive:\Attachments").Path
+        $expectedPath = (Resolve-Path -Path "TestDrive:/Attachments").Path
         $userPreferences = Get-Content $BloggerSession.UserPreferences -Raw | ConvertFrom-Json
         $userPreferences.AttachmentsDirectory | Should -Be $expectedPath
       }
